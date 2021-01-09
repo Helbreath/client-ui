be8063be4f32a617743243e113fb9cb92322f87c
Sharon Fox <sharon@sharonfox.dev>
2021-01-09 04:34:54 -0500

    Finished waiting response screen

--- src/components/WaitingResponse.vue
+++ src/components/WaitingResponse.vue
@@ -4,6 +4,17 @@
       Waiting for server response...<br />
       <v-btn v-if="hasCancel" class="text-center mt-2" @click="CancelWaiting()">Cancel waiting</v-btn>
     </div>
+
+    <v-dialog v-model="creationError" persistent max-width="450">
+      <v-card>
+        <v-card-title class="text-h6">Error</v-card-title>
+        <v-card-text>{{ errorMessage }}</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success darken-1" text @click="CallbackSuccess">Okay</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 <script lang="ts">
@@ -17,12 +28,34 @@ export default class WaitingResponse extends Vue {
   private timeStarted: number;
   private hasCancel: boolean = false;
   private cancelTimeout!: number;
+  private creationError: boolean = false;
+  private errorMessage: string = '';
+
   constructor() {
     super();
     this.timeStarted = Date.now();
   }
+  protected CallbackSuccess() {
+    this.$root.ChangeGameMode('mainmenu');
+  }
+  protected LogResMsg(msg: any) {
+    window.log(msg);
+    if (msg.status === 'failed') {
+      this.errorMessage = 'Disconnected from server: ' + msg.reason;
+      this.creationError = true;
+      this.$root.ChangeMouse(0);
+      return;
+    }
+    if (msg.status === 'success') {
+      return;
+    }
+
+    const one = msg.charAt(0);
+    const two = msg.charAt(1);
+  }
   protected CancelWaiting() {
     window.SendMessage('cancelwaiting', {});
+    this.$root.ChangeGameMode('mainmenu');
     this.$root.PlaySound('E', 14, 5);
   }
   protected AddCancel() {
@@ -33,7 +66,11 @@ export default class WaitingResponse extends Vue {
     this.cancelTimeout = setTimeout(this.AddCancel, 5000);
   }
   protected beforeDestroy() {
+    this.$root.$off('logresmsg', this.LogResMsg);
     if (this.cancelTimeout) clearTimeout(this.cancelTimeout);
+  }
+  protected created() {
+    this.$root.$on('logresmsg', this.LogResMsg);
   }
 }
 </script>
